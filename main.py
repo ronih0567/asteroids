bc7feb0220042f383e5ba93d38e6620ce7283a3d
Ron Ih <ronih@pacbell.net>
2025-12-15 00:11:56 -0800

    J:Added shot-asteroid collision detection

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
         screen.fill("black") 
         updatable.update(dt)
         for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print("Game over!")
